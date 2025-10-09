cbb811d858c327a03593965160ea4586e0081034
Andrey Semenov <i@itmage.ru>
2025-10-09 23:12:23 +0300

    chore: added logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod handler;
 mod html;
 
+use tracing_subscriber::{
+    fmt::format::{FmtSpan, Pretty},
+    prelude::*,
+};
+
+use wasm_bindgen::JsValue;
 use yew::prelude::*;
 
 // https://core.telegram.org/bots/webapps#initializing-mini-apps
@@ -24,5 +30,18 @@ fn app() -> Html {
 }
 
 fn main() {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(false)
+        .with_writer(tracing_web::MakeConsoleWriter)
+        .without_time()
+        .with_span_events(FmtSpan::ACTIVE);
+    let perf_layer = tracing_web::performance_layer().with_details_from_fields(Pretty::default());
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init();
+    let object = JsValue::from("Starting app...");
+    tracing::info!("Hello {}", object.as_string().unwrap());
+
     yew::Renderer::<App>::new().render();
 }
