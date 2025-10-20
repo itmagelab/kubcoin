41a1d286671fce9842e33fd779a0d52cae49a366
Andrey Semenov <i@itmage.ru>
2025-10-20 19:38:23 +0300

    refactor(i18n): simplify language switcher to button toggle
    - Replace dropdown menu with simple button toggle
    - Use 'Eng | Ru' text instead of full language names
    - Remove flag emojis for cleaner design
    - Implement button group with has-addons class
    - Active language highlighted with is-link style
    - Inactive language uses is-ghost style
    - Maintain ARIA labels for accessibility
    - Simpler UX with direct language switching

--- src/html/language_switcher.rs
+++ src/html/language_switcher.rs
@@ -3,107 +3,58 @@
 use crate::i18n::{Language, use_language};
 use yew::prelude::*;
 
-/// Language switcher component with dropdown menu
+/// Language switcher component with button toggle
 #[function_component(LanguageSwitcher)]
 pub(crate) fn language_switcher() -> Html {
     let ctx = use_language();
     let current_lang = ctx.language;
-    let is_open = use_state(|| false);
 
-    let toggle_dropdown = {
-        let is_open = is_open.clone();
+    let switch_to_russian = {
+        let set_language = ctx.set_language.clone();
         Callback::from(move |e: MouseEvent| {
             e.prevent_default();
-            is_open.set(!*is_open);
+            set_language.emit(Language::Russian);
         })
     };
 
-    let close_dropdown = {
-        let is_open = is_open.clone();
-        Callback::from(move |_| {
-            is_open.set(false);
-        })
-    };
-
-    let on_language_select = {
+    let switch_to_english = {
         let set_language = ctx.set_language.clone();
-        let is_open = is_open.clone();
-
-        Callback::from(move |lang: Language| {
-            set_language.emit(lang);
-            is_open.set(false);
+        Callback::from(move |e: MouseEvent| {
+            e.prevent_default();
+            set_language.emit(Language::English);
         })
     };
 
-    let dropdown_class = if *is_open {
-        "dropdown is-active"
+    let ru_class = if current_lang == Language::Russian {
+        "button is-small is-link"
     } else {
-        "dropdown"
+        "button is-small is-ghost"
+    };
+
+    let en_class = if current_lang == Language::English {
+        "button is-small is-link"
+    } else {
+        "button is-small is-ghost"
     };
 
     html! {
-        <div class={dropdown_class}>
-            <div class="dropdown-trigger">
-                <button
-                    class="button is-light"
-                    aria-haspopup="true"
-                    aria-controls="language-menu"
-                    aria-label="Select language"
-                    onclick={toggle_dropdown}
-                >
-                    <span class="icon is-small">
-                        <span>{ current_lang.flag() }</span>
-                    </span>
-                    <span>{ current_lang.native_name() }</span>
-                    <span class="icon is-small">
-                        <i class="fas fa-angle-down" aria-hidden="true"></i>
-                    </span>
-                </button>
-            </div>
-            <div class="dropdown-menu" id="language-menu" role="menu">
-                <div class="dropdown-content">
-                    { for Language::all().iter().map(|&lang| {
-                        let on_click = {
-                            let on_language_select = on_language_select.clone();
-                            Callback::from(move |e: MouseEvent| {
-                                e.prevent_default();
-                                on_language_select.emit(lang);
-                            })
-                        };
-
-                        let item_class = if lang == current_lang {
-                            "dropdown-item is-active"
-                        } else {
-                            "dropdown-item"
-                        };
-
-                        html! {
-                            <a
-                                href="#"
-                                class={item_class}
-                                onclick={on_click}
-                                role="menuitem"
-                            >
-                                <span class="icon-text">
-                                    <span class="icon">
-                                        <span>{ lang.flag() }</span>
-                                    </span>
-                                    <span>{ lang.native_name() }</span>
-                                </span>
-                            </a>
-                        }
-                    }) }
-                </div>
-            </div>
-
-            // Close dropdown when clicking outside
-            if *is_open {
-                <div
-                    class="modal-background"
-                    style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background: transparent; z-index: 10;"
-                    onclick={close_dropdown}
-                />
-            }
+        <div class="buttons has-addons" style="margin-bottom: 0;">
+            <button
+                class={en_class}
+                onclick={switch_to_english}
+                aria-label="Switch to English"
+                aria-pressed={if current_lang == Language::English { "true" } else { "false" }}
+            >
+                { "Eng" }
+            </button>
+            <button
+                class={ru_class}
+                onclick={switch_to_russian}
+                aria-label="Переключить на русский"
+                aria-pressed={if current_lang == Language::Russian { "true" } else { "false" }}
+            >
+                { "Ru" }
+            </button>
         </div>
     }
 }
